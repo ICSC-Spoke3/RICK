0052704ea67d51445d44d865fe41eee9d4a74f67
GiovanniINAF <giovanni.lacopo@inaf.it>
2025-04-04 12:50:55 +0200

    remove unused files

--- create_so.bash
+++
@@ -1,3 +0,0 @@
-mpicc -fopenmp -shared -o gridding.so -fPIC -DKAISERBESSEL gridding_library.c
-mpicc -fopenmp -shared -o fft.so -fPIC fft_library.c -I/usr/local/include -L/usr/local/lib -lfftw3_omp -lfftw3_mpi -lfftw3
-mpicc -fopenmp -shared -o phasecorr.so -fPIC phase_correction_library.c
